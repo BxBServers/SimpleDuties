dcb348b86a25b25001c799f7d56b849d7585cc4a
Randomfish132 <a12345678910ster@gmail.com>
2016-12-03 15:11:56 +0800

    Add vault

--- src/main/java/com/bxbservers/bukkit/simpleduties/Main.java
+++ src/main/java/com/bxbservers/bukkit/simpleduties/Main.java
@@ -1,12 +1,31 @@
 package com.bxbservers.bukkit.simpleduties;
 
+import org.bukkit.Bukkit;
+import net.milkbowl.vault.permission.Permission;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin
 {
+   private Permission permission;
+
    @Override
    public void onEnable()
    {
+      if( !setupPermissions() ) {
+         getLogger().severe( "Could not get Vault permission provider, disabling plugin." );
 
+         Bukkit.getPluginManager().disablePlugin( this );
+      }
+   }
+
+   private boolean setupPermissions()
+   {
+      RegisteredServiceProvider<Permission> provider = Bukkit.getServicesManager().getRegistration( Permission.class );
+
+      if( provider != null )
+         permission = provider.getProvider();
+
+      return permission != null;
    }
 }
