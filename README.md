2b1eb674ee5947233588caf4800d58994964bde7
Randomfish132 <a12345678910ster@gmail.com>
2016-12-03 14:53:05 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+SimpleDuties
+=============
+
+ezpz
---
+++ src/main/java/com/bxbservers/bukkit/simpleduties/Main.java
@@ -0,0 +1,12 @@
+package com.bxbservers.bukkit.simpleduties;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin
+{
+   @Override
+   public void onEnable()
+   {
+
+   }
+}
